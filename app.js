be1d7eb08938b50e77366801f2f9c35270dff333
Antonio <antoniofebrero24@hotmail.com>
2026-02-19 16:28:27 +0100

    update dolar and bs rate

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ let count = 3;
 let method = "";
 let copyTxt = "";
 let orderID = "";
-const PRICE_USD = 1.5;
-const RATE_BS = 630; // ⬅️ SOLO CAMBIAS ESTO CADA SEMANA
+const PRICE_USD = 1.3;
+const RATE_BS = 585; // ⬅️ SOLO CAMBIAS ESTO CADA SEMANA
 
 const paymentMethods = {
     "Pago Móvil": {
